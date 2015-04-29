9c757b49b4096fc571dc6c4e4b90a80c8320c358
f.ruguo <f.ruguo@gmail.com>
2015-04-29 16:17:23 +0800

    add ipv4

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -8,9 +8,21 @@ var less = require('gulp-less');
 var path = require('path');
 var minifyCSS = require('gulp-minify-css');
 var server = require('gulp-server-livereload');
+var os = require('os');
+
+function getIpv4(){
+  var ipv4, en6;
+  en6 = os.networkInterfaces().en6;
+  en6.forEach(function(item){
+    if(item.family === 'IPv4'){
+      ipv4 = item.address;
+    }
+  });
+  return ipv4;
+}
 
 var paths = {
-	less: ['./less/**/*.less']
+  less: ['./less/**/*.less']
 };
 
 gulp.task('less', function () {
@@ -23,12 +35,13 @@ gulp.task('less', function () {
 });
 
 gulp.task('watch', function() {
-	gulp.watch(paths.less, ['less']);
+  gulp.watch(paths.less, ['less']);
 });
 
 gulp.task('webserver', function() {
   gulp.src(__dirname)
     .pipe(server({
+      host: getIpv4(),
       port: 8888,
       livereload: true,
       directoryListing: true,
@@ -37,6 +50,6 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('default', ['less', 'watch', 'webserver'], function(event) {
-	console.log(event);
-	console.log('Less Files was running tasks...');
+  console.log(event);
+  console.log('Less Files was running tasks...');
 });
